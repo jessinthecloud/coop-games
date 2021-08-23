e046312ef48e9d8757b3a1efead5dc73bbc4d947
Jess <code@jessinthe.cloud>
2021-08-23 18:23:15 -0400

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // so Tailwind can tree-shake unused styles in production builds
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
