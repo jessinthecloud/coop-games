bcc31638ff85f66af19d064785bc1987eb664607
jessinthecloud <git@github.com>
2021-08-24 22:39:39 -0400

    add /games to games routes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [GameController::class, 'index'])->name('home');
-Route::get('/{slug}', [GameController::class, 'show'])->name('games.show');
+
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/{slug}', [GameController::class, 'show'])->name('games.show');
 Route::get('/platforms/{slug}', [PlatformController::class, 'show'])->name('platforms.show');
