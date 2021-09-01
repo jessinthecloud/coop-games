c6b7db4aac8ecdb62552438b5ce947455f7dbf16
jessinthecloud <git@github.com>
2021-08-31 20:36:00 -0400

    close #36 - make header and footer views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@include('layouts.header')
 
-        <title>{{ config('app.name') }}</title>
+    <x-navigation></x-navigation>
 
-{{--        <!-- Fonts -->--}}
-{{--        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
-
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/common.css') }}" rel="stylesheet">
-
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="antialiased w-full flex flex-col text-gray-200">
-
-        <x-navigation></x-navigation>
-
-        <div class="container min-h-screen flex flex-column justify-center items-start mx-auto
-        ">
-            <div id="content-wrapper">
-                @yield('content')
-            </div>
-            <!-- #content-wrapper -->
+    <div
+        class="container min-h-screen flex flex-column justify-center items-start mx-auto"
+    >
+        <div id="content-wrapper">
+            @yield('content')
         </div>
+        <!-- #content-wrapper -->
+    </div>
 
-        <script src="https://cdn.rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js"></script>
-
-        {{-- allows other views to also add JS --}}
-        @stack('scripts')
-    </body>
-</html>
+@include('layouts.footer')
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+    <script src="https://cdn.rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js"></script>
+
+    {{-- allows other views to also add JS --}}
+    @stack('scripts')
+</body>
+</html>
