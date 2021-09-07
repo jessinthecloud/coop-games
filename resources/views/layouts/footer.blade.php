2503d4ad622bfd797dd587d0dfbba6e7a3796ef1
jessinthecloud <git@github.com>
2021-09-07 18:41:18 -0400

    add livewire

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,7 @@
-    <script src="https://cdn.rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js"></script>
+    @livewireScripts
 
+    <script src="https://cdn.rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js"></script>
+    
     {{-- allows other views to also add JS --}}
     @stack('scripts')
 </body>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,6 +10,8 @@
 {{--        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
 
 <!-- Styles -->
+    @livewireStyles
+    
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/common.css') }}" rel="stylesheet">
