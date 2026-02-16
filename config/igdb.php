039df14e340032c0292dd8ad3ca900452c7b8254
Jess <jess@DESKTOP-JE3SDJT.localdomain>
2026-02-16 13:22:48 -0500

    workaround composer error

--- config/igdb.php
+++ config/igdb.php
@@ -5,7 +5,7 @@ return [
      * These are the credentials you got from https://dev.twitch.tv/console/apps
      */
     'credentials' => [
-        'client_id' => env('TWITCH_CLIENT_ID'),
+        'client_id' => env('TWITCH_CLIENT_ID', ''),
         'client_secret' => env('TWITCH_CLIENT_SECRET'),
     ],
 
